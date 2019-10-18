bc9ebea15780995298a290838878eabedfe252f0
Monica Toader <toadermonica@gmail.com>
2019-10-18 16:30:54 +0200

    client server non-dynamic thread allocation

---
+++ Client/RunClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Client
+{
+    class RunClient
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            string ipAddress = "127.0.0.1";
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                client.Connect(ipAddress, "Hello I'm Device 1...");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                client.Connect(ipAddress, "Hello I'm Device 2...");
+            }).Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/RunServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Server
+{
+    class RunServer
+    {
+        static void Main(string[] args)
+        {
+            string ipAddress = "127.0.0.1";
+            Thread t = new Thread(delegate ()
+            {
+                // replace the IP with your system IP Address...
+                Server myserver = new Server(ipAddress, 13000);
+            });
+            t.Start();
+
+            Console.WriteLine("Server Started...!");
+        }
+    }
+}
