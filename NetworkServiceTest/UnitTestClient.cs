dcc7c63312be5ceceb5e428ddb74a55c1d947a52
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-10-19 20:35:43 +0200

    Added Tests

---
+++ NetworkServiceTest/UnitTestClient.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Client;
+
+namespace Tests
+{
+    public class UnitTestClient
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ NetworkServiceTest/UnitTestServer.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Server;
+
+namespace Tests
+{
+    public class UnitTestServer
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
