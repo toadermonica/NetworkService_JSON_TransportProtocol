9156412c9ca78847330ee7e404b1e0563f2c30f2
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-10-22 18:30:08 +0200

    Create Status.cs

---
+++ Server2/Status.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class Status
+    {
+        public int code { get; set; }
+        public string statusBody { get; set; }
+
+        public string SetStatusBody(int code)
+        {
+            return code switch
+            {
+                1 => "OK",
+                2 => "CREATED",
+                3 => "UPDATED",
+                4 => "BAD REQUEST",
+                5 => "NOT FOUND",
+                6 => "ERROR",
+                _ => "UNKNOWN STATUS CODE",
+            };
+        }
+    }
+}
